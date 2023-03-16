ded2cb20fd38b3b8677666c276f97b79d5a401f2
Alesia-V175 <vaivasilisa@gmail.com>
2023-03-16 17:45:42 +0300

    feat: add footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import { NavLink, Outlet } from 'react-router-dom';
 
 const Layout = () => (
-  <div>
+  <>
     <header>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About us</NavLink>
@@ -11,19 +11,22 @@ const Layout = () => (
       <Outlet />
     </main>
 
-    <footer>
-      <ul className="footer__list">
+    <footer className="footer">
+      <ul className="footer__list container">
         <li className="footer__item">
-          <a href="https://github.com/Alesia-V175" target="_blank" className="link github-logo" rel="noreferrer"></a>
+          <a href="https://github.com/Alesia-V175" target="_blank" className="link github-logo" rel="noreferrer">
+            <img src='../../src/assets/icons/github.svg'></img>
+          </a>
         </li>
         <li className="footer__item">© by Alesia Sokolovskaya 2023</li>
         <li className="footer__item">
           <a href="https://rs.school/js/" target="_blank" className="link" rel="noreferrer">
+            <img className="rs-logo" src="../../src/assets/icons/rs-school.svg" alt="RS School" title="RS School" />
           </a>
         </li>
       </ul>
     </footer>
-  </div>
+  </>
 );
 
 export default Layout;
