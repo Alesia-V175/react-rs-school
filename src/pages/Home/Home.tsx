b6a4cc0df75b0fc21af0d78459dcb2175d7c35a7
Alesia-V175 <vaivasilisa@gmail.com>
2023-03-19 12:57:55 +0300

    feat: implement saving input value to localStarage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import { ICardItem } from '../../types/interfaces';
 import CardsList from '../../components/CardsList/CardsList.js';
 import cardsDB from '../../Database/database.json';
 import styles from './Home.module.scss';
+import Search from '../../components/Search/Search.js';
 
 class Home extends React.Component<object, { cards: ICardItem[] }> {
   state = {
@@ -22,6 +23,7 @@ class Home extends React.Component<object, { cards: ICardItem[] }> {
         </div>
         <div className={styles.main__content}>
           <h1 className={styles.main__title}>Explore the world with a photo!</h1>
+          <Search />
           <section className={styles.main__cards}>
             <CardsList cards={this.state.cards} />
           </section>
