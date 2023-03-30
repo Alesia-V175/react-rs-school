6eea93044feee69d1db552480817241dad992928
Alesia-V175 <vaivasilisa@gmail.com>
2023-03-30 16:22:35 +0300

    feat: update search bar

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,55 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { KeyLocalStorage } from '../../constants/constants';
 import styles from './Search.module.scss';
 
-class Search extends React.Component {
-  state = {
-    search: '',
-  };
+const Search = ():JSX.Element => {
+  const [searchValue, setSearchValue] = useState(() => {
+    const savedSearchValue = localStorage.getItem(KeyLocalStorage.search);
 
-  private static getSearchValue(): string {
-    const searchValue = localStorage.getItem(KeyLocalStorage.search);
-
-    if (searchValue) {
-      return JSON.parse(searchValue);
+    if (savedSearchValue) {
+      return JSON.parse(savedSearchValue);
     }
 
     return '';
-  }
+  });
 
-  private updateSearchValue = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      search: event.target.value,
-    });
+  useEffect(() => {
+    localStorage.setItem(KeyLocalStorage.search, JSON.stringify(searchValue));
+  }, [searchValue]);
 
-    localStorage.setItem(KeyLocalStorage.search, JSON.stringify(event.target.value));
-  };
-
-  componentDidMount() {
-    this.setState({
-      search: Search.getSearchValue(),
-    });
-  }
-
-  componentWillUnmount() {
-    if (this.state.search) {
-      localStorage.setItem(KeyLocalStorage.search, JSON.stringify(this.state.search));
-    }
-  }
-
-  render() {
-    return (
-      <div className={styles.search}>
-        <input
-          type="search"
-          placeholder="Search a photo"
-          className={styles.search__input}
-          value={this.state.search}
-          onChange={this.updateSearchValue}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.search}>
+      <input
+        type="search"
+        placeholder="Search a photo"
+        className={styles.search__input}
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
+      />
+    </div>
+  );
+};
 
 export default Search;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,37 +1,27 @@
-import React from 'react';
-import { ICardItem } from '../../types/interfaces';
+import React, { useState } from 'react';
 import CardsList from '../../components/CardsList/CardsList';
 import cardsDB from '../../database/database.json';
 import Search from '../../components/Search/Search';
 import banner from '../../assets/images/banner-main.jpg';
 import styles from './Home.module.scss';
 
-class Home extends React.Component<object, { cards: ICardItem[] }> {
-  state = {
-    cards: [],
-  };
+const Home = (): JSX.Element => {
+  const [cards] = useState(cardsDB);
 
-  componentDidMount() {
-    const db: ICardItem[] = cardsDB;
-    this.setState({ cards: db });
-  }
-
-  render() {
-    return (
-      <>
-        <div className={styles.main__banner}>
-          <img src={banner} alt="banner" className={styles.main__banner_img}/>
-        </div>
-        <div className={styles.main__content}>
-          <h1 className={styles.main__title}>Explore the world with a photo!</h1>
-          <Search />
-          <section className={styles.main__cards}>
-            <CardsList cards={this.state.cards} />
-          </section>
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div className={styles.main__banner}>
+        <img src={banner} alt="banner" className={styles.main__banner_img}/>
+      </div>
+      <div className={styles.main__content}>
+        <h1 className={styles.main__title}>Explore the world with a photo!</h1>
+        <Search />
+        <section className={styles.main__cards}>
+          <CardsList cards={cards} />
+        </section>
+      </div>
+    </>
+  );
+};
 
 export default Home;
